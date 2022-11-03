47c4673e346c719a44c22d9ba7c25465f4273311
JoseManuelFrancoAlzate <josemanuelalzatefranco12@gmail.com>
2022-11-03 18:35:52 -0500

    Hola

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,83 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    
+
+    var array =  [1,2,3,4,5,6,7,8,9,10];
+
+
+
+    var suma = 0;
+
+    for (var i = 0; i < array.length ; i ++){
+
+suma += array[i]
+
+    }
+   
+    return suma
+    
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+ 
+  var pares = array.filter(numeros => numeros % 2 === 0)
+
+  return pares
+
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+
+
+const cuadrado = array.map(function(elevados){
+	
+	
+	 return  elevados ** 2
+	
+})
+
+return cuadrado
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+
+  let maquinaAcumuladora = array.reduce((acumulador,numeroActual) => {
+
+    return acumulador + numeroActual
+    
+    
+        },)
+    
+        return maquinaAcumuladora
+/*
+  var suma = 0
+
+for(var i = 0; i < array.length; i ++) {
+
+  suma += array[i]
+
+}
+
+return suma
+*/
+
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+
+  return num.toString().length;
 }
   
   
